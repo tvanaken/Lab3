27467ef32a964f649e17bc4b5633cfae1145091a
tvanaken <89529903+tvanaken@users.noreply.github.com>
2022-02-06 19:21:51 -0500

    Finished

--- src/edu/unca/csci202/ArrayPalindrome.java
+++ src/edu/unca/csci202/ArrayPalindrome.java
@@ -6,13 +6,28 @@ public class ArrayPalindrome {
 	ArrayStack<String> stack2 = new ArrayStack<String>();
 	ArrayStack<String> stack3 = new ArrayStack<String>();
 
-	public boolean isPalindrome(String testPalindrome) {
-
+	public boolean isPalindrome(String testPalindrome) throws EmptyCollectionException{
+		
 		for (int i = 0; i < testPalindrome.length(); i++) {
 			
+			if (Character.toString(testPalindrome.charAt(i)).isEmpty()) {
+				
+				throw new EmptyCollectionException(testPalindrome);
+			}
+			
 			String letter = Character.toString(testPalindrome.charAt(i));
+			
+			if (letter.isBlank()) {
+				
+				throw new EmptyCollectionException("stack");
+			}
 			stack1.push(letter);
 			stack2.push(letter);
+		}
+		
+		if (stack1.size() == 0) {
+			
+			throw new EmptyCollectionException(stack1.toString());
 		}
 		
 		for (int i = 0; i < testPalindrome.length(); i++) {
@@ -20,22 +35,41 @@ public class ArrayPalindrome {
 			stack3.push(stack2.pop());
 		}
 		
-		while (true) {
+		while (!stack1.isEmpty()) {
 			
 			for (int i = 0; i < testPalindrome.length(); i++) {
 				
-				if (stack1.pop() == stack3.pop()) {
+				String x = stack1.pop();
+				String y = stack3.pop();
+				
+				if (x.equals(y)) {
 					
-					return true;
+					continue;
 				} else {
-					
+								
+					System.out.println(testPalindrome + " is not a palindrome.");
+					clear(testPalindrome.length());
 					return false;
 				}
 			}
-			
-			return true;
 		}
 		
-		return false;
+		System.out.println(testPalindrome + " is a palindrome.");
+		clear(testPalindrome.length());
+		return true;
+	}
+	
+	public void clear(int palLength) {
+		
+		try {
+			for (int i = 0; i < palLength; i++) {
+				
+				stack1.pop();
+				stack3.pop();
+			}
+		} catch (EmptyCollectionException e) {
+			
+		}
+		
 	}
 }
--- src/edu/unca/csci202/Main.java
+++ src/edu/unca/csci202/Main.java
@@ -5,10 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		ArrayPalindrome test = new ArrayPalindrome();
-		test.isPalindrome("Mom");
-		System.out.println(test.stack1);
-		System.out.println(test.stack2);
-		System.out.println(test.stack3);
+		test.isPalindrome("mom");
+		test.isPalindrome("Racecar");
+		test.isPalindrome("   ");
 	}
 
 }
